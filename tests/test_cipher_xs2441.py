25c2b582f8cf310da1c752fa141d8f77ba7c9495
alanshen21 <85988982+alanshen21@users.noreply.github.com>
2021-11-10 18:31:55 -0500

    Update test file

--- tests/test_cipher_xs2441.py
+++ tests/test_cipher_xs2441.py
@@ -1,1 +1,8 @@
 from cipher_xs2441 import cipher_xs2441
+
+def test_cipher(): 
+    example = 'Apple'
+    expected = 'Dssoh'
+    actual = cipher_xs2441.cipher(example, 3)
+    assert actual == expected
+    
